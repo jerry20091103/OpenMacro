44e683a12aa77e2c043a1771d2f1795666139db5
CHM <peterconnected@gmail.com>
2022-06-06 00:13:44 +0800

    Specified write size

--- OpenMacro-GUI/Core/preset.cpp
+++ OpenMacro-GUI/Core/preset.cpp
@@ -197,7 +197,7 @@ void Preset::uploadToSerial(QSerialPort& serialPort)
             dataPtr += sizeof(MacroPacket) * input.packets.size();
         }
         // TODO: Fill in expander address later
-        int bytesWritten = serialPort.write((const char*)&data);
+        int bytesWritten = serialPort.write((const char*)&data, sizeof(MacroConfig));
 
         if(bytesWritten == -1) {
             throw "Failed to write to port " + serialPort.portName() + ", error: " + serialPort.errorString();
