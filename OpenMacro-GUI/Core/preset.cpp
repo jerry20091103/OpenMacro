b862d9d9e50ff5a4c956f2a4490e2f7b61ce7ed7
CHM <peterconnected@gmail.com>
2022-06-06 11:00:21 +0800

    Use std string -> char pointer instead

--- OpenMacro-GUI/Core/preset.cpp
+++ OpenMacro-GUI/Core/preset.cpp
@@ -199,7 +199,7 @@ void Preset::uploadToSerial(QSerialPort& serialPort)
             dataPtr += sizeof(MacroPacket) * input.packets.size();
         }
         // TODO: Fill in expander address later
-        int bytesWritten = serialPort.write(QByteArray::fromRawData((const char*)&data, sizeof(MacroConfig)), sizeof(MacroConfig));
+        int bytesWritten = serialPort.write(std::string((const char*)&data, sizeof(MacroConfig)).c_str());
         if(bytesWritten == -1) {
             throw "Failed to write to port " + serialPort.portName() + ", error: " + serialPort.errorString();
         }
